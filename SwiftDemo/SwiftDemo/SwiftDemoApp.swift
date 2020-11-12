46de2aabc949f0638c09b127498d6de4298090e8
zhenzi <tianzhenzi@inke.cn>
2020-11-12 10:44:48 +0800

    Add SwiftDemo

---
+++ SwiftDemo/SwiftDemo/SwiftDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDemoApp.swift
+//  SwiftDemo
+//
+//  Created by Tian on 2020/11/12.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
